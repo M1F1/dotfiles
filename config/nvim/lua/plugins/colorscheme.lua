fce9fd1c04df83a6cf4bd37fa4245bf31509bb64
Michał Fiłek <michal.filek@tesco.com>
2024-09-14 13:00:34 +0200

    add doom colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,39 @@
 return {
-  "0xstepit/flow.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("flow").setup({
-      dark_theme = true,
-      transparent = false,
-      high_contrast = false,
-      fluo_color = "orange",
-      mode = "desaturate",
-      aggressive_spell = false,
-    })
-    vim.cmd("colorscheme flow")
-  end,
+  {
+    "NTBBloodbath/doom-one.nvim",
+    setup = function()
+      -- Add color to cursor
+      vim.g.doom_one_cursor_coloring = false
+      -- Set :terminal colors
+      vim.g.doom_one_terminal_colors = true
+      -- Enable italic comments
+      vim.g.doom_one_italic_comments = false
+      -- Enable TS support
+      vim.g.doom_one_enable_treesitter = true
+      -- Color whole diagnostic text or only underline
+      vim.g.doom_one_diagnostics_text_color = false
+      -- Enable transparent background
+      vim.g.doom_one_transparent_background = false
+
+      -- Pumblend transparency
+      vim.g.doom_one_pumblend_enable = false
+      vim.g.doom_one_pumblend_transparency = 20
+
+      -- Plugins integration
+      vim.g.doom_one_plugin_neorg = true
+      vim.g.doom_one_plugin_barbar = false
+      vim.g.doom_one_plugin_telescope = false
+      vim.g.doom_one_plugin_neogit = true
+      vim.g.doom_one_plugin_nvim_tree = true
+      vim.g.doom_one_plugin_dashboard = true
+      vim.g.doom_one_plugin_startify = true
+      vim.g.doom_one_plugin_whichkey = true
+      vim.g.doom_one_plugin_indent_blankline = true
+      vim.g.doom_one_plugin_vim_illuminate = true
+      vim.g.doom_one_plugin_lspsaga = false
+    end,
+    config = function()
+      vim.cmd("colorscheme doom-one")
+    end,
+  },
 }
